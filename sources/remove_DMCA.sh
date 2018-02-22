8b0980d1e3f494f3cb24e4ed14648f19098b2dac
darksun <lujun9972@gmail.com>
2018-02-22 16:43:53 +0800

    add done: 20180222 3 reasons to say -no- in DevOps.md

--- sources/remove_DMCA.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-domain="$1"
-git checkout -b "$domain"
-git grep -l "$domain"|while read file; do git rm "$file"; done
-git commit -a -m "remove $domain"
-git push -u origin "$domain"
-git checkout master
