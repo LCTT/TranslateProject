a1414469215a4c375fe9d697a0a33a938d17dc7d
darksun <lujun9972@gmail.com>
2018-11-23 22:12:56 +0800

    添加cron CI

--- scripts/status/status.sh
+++ scripts/status/status.sh
@@ -13,12 +13,15 @@ function get_status_of()
     git log --date=short --pretty=format:"{\"file\":\"${file}\",\"time\":\"%ad\",\"user\":\"%an\"}" -n 1  "${file}"
 }
 
-(
-git grep -niE "translat|fanyi|翻译"  sources/*.md |awk -F ":" '{if ($2<=3) print $1}' |xargs -I{} git log --date=short --pretty=format:"{\"filename\":\"{}\",\"time\":\"%ad\",\"user\":\"%an\"}" -n 1  "{}"|jq --slurp
-
+translating=$( git grep -niE "translat|fanyi|翻译"  sources/*.md |awk -F ":" '{if ($2<=3) print $1}' |xargs -I{} git log --date=short --pretty=format:"{\"filename\":\"{}\",\"time\":\"%ad\",\"user\":\"%an\"}" -n 1  "{}")
+unselected=$(
 find sources -name "2*.md"|sort|while read file;do
     if ! file-translating-p "${file}";then
         get_status_of "${file}"
     fi
-done |jq --slurp
-)|jq --slurp '{"translating":.[0],"unselected":.[1]}'
+done
+)
+(
+echo ${translating}|jq -s "."
+echo ${unselected} |jq -s "."
+)|jq -s '{"translating":.[0],"unselected":.[1]}'
